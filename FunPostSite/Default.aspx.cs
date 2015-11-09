fd9e00593a309e3407cc873db2f40ea8776737a3
wukuan84 <leoisawe@gmail.com>
2015-11-08 23:43:54 -0500

    store files into local directory of this website project

--- FunPostSite/Default.aspx.cs
+++ FunPostSite/Default.aspx.cs
@@ -9,7 +9,13 @@ using System.IO;
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
+     {
+          DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/Appdata"));
+          var directories = di.GetFiles("*", SearchOption.AllDirectories);
+          foreach (FileInfo d in directories)
+          {
+               Post.
+          }
     }
 
 
@@ -23,6 +29,10 @@ public partial class _Default : System.Web.UI.Page
         newPost.Visible = false;
         String t = title.Value;
         HttpPostedFile image=pic.PostedFile;
+        int s=image.FileName.IndexOf(".");
+        String r = image.FileName.Substring(s);
+        var path = Path.Combine(Server.MapPath("~/Appdata"), t+r);
+        image.SaveAs(path);
     }
 
 }
