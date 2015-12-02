6b8323c8e09ad97bc94e0221355f7207af50a420
Leo <leoisawe@gmail.com>
2015-12-02 00:52:33 -0500

    Unit Testing

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Testvalidate()
+        {
+            HttpPostedFile a;
+        }
+    }
+}
